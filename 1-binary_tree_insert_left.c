786cf102ef758ae0b5dedfd134c290a5474cb9a4
saadelhamidi <saadavicii123@gmail.com>
2024-06-06 13:31:48 -0700

    x

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * @value: The value to store in the node.
  * Return: NULL if it fails or the new node
  */
-binary_tree_t *binary_tree_inserting_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n_node;
 
