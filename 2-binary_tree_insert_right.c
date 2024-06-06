1b82ae8ce084089345003c09c7d2854b527b862b
saadelhamidi <saadavicii123@gmail.com>
2024-06-06 13:46:42 -0700

    x

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,21 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *n_node;
+	binary_tree_t *n_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    n_node = binary_tree_node(parent, value);
-    if (n_node == NULL)
-        return (NULL);
+	n_node = binary_tree_node(parent, value);
+	if (n_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        n_node->right = parent->right;
-        parent->right->parent = n_node;
-    }
-    parent->right = n_node;
+	if (parent->right != NULL)
+	{
+		n_node->right = parent->right;
+		parent->right->parent = n_node;
+	}
+	parent->right = n_node;
 
-    return (n_node);
+	return (n_node);
 }
